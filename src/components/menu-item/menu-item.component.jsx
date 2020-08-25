82bb4b68d39de4c7bd987ecff2e2737527f029ff
RoseAppleMedia <michael@roseapplemedia.com>
2020-08-25 14:45:04 -0400

    updated router navigation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-export default function MenuItem({ title, imageUrl, size }) {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-        className="background-image"
-      ></div>
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">Shop Now</span>
-      </div>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Shop Now</span>
     </div>
-  );
-}
+  </div>
+);
+export default withRouter(MenuItem);
