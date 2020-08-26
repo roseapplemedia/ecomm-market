62371253c4c88a4b8e1747866322f1461b09f613
RoseAppleMedia <michael@roseapplemedia.com>
2020-08-26 14:26:07 -0400

    added styles and sign-in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import "./app.css";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
+import SignInAndSignOut from "./components/sign-in-and-sign-out/sign-in-and-sign-out.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignOut} />
       </Switch>
     </div>
   );
