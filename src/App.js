18dc6e0ec6af64a9d61554a68b4123669aefd12a
RoseAppleMedia <michael@roseapplemedia.com>
2020-08-25 22:39:31 -0400

    styles preview and homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import "./app.css";
+import ShopPage from "./pages/shop/shop.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
